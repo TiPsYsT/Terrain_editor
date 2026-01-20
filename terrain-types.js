1e7779ef23c51ff17b54dab1288f079534479816
TiPsYsT <alexanderlundgren@hotmail.com>
2026-01-20 15:52:51 +0100

    Update terrain-types.js

--- terrain-types.js
+++ terrain-types.js
@@ -1,27 +1,65 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8" />
-  <title>Terrain Editor</title>
-  <link rel="stylesheet" href="style.css" />
-</head>
-<body>
+export const INCH = 25.4;
 
-<div id="toolbar">
-  <button id="add-two">Add 2-storey ruin (red)</button>
-  <button id="add-three">Add 3-storey ruin (blue)</button>
-  <button id="add-proto">Add prototype ruin (gray)</button>
-  <button id="add-container">Add container (gray)</button>
-  <button id="rot-left">Rotate -45°</button>
-  <button id="rot-right">Rotate +45°</button>
-  <button id="export">Export JSON</button>
-</div>
+/*
+  Enda sanningen:
+  - endast geometri
+  - färg = visuell identitet
+*/
 
-<canvas id="board" width="1524" height="1118"></canvas>
+export const TERRAIN_TYPES = {
+  /* ===== RUINS ===== */
 
-<script type="module">
-  import "./editor.js";
-</script>
+  two_storey_ruin_12x6: {
+    id: "two_storey_ruin_12x6",
+    type: "ruin",
+    color: "red",
+    w: 12 * INCH,
+    h: 6 * INCH
+  },
 
-</body>
-</html>
+  three_storey_ruin_12x6: {
+    id: "three_storey_ruin_12x6",
+    type: "ruin",
+    color: "blue",
+    w: 12 * INCH,
+    h: 6 * INCH
+  },
+
+  prototype_ruin_6x5: {
+    id: "prototype_ruin_6x5",
+    type: "ruin",
+    color: "gray",
+    w: 6 * INCH,
+    h: 5 * INCH
+  },
+
+  /* ===== CONTAINER ===== */
+
+  armoured_container_5x2_5: {
+    id: "armoured_container_5x2_5",
+    type: "container",
+    color: "gray",
+    w: 5 * INCH,
+    h: 2.5 * INCH
+  }
+};
+
+/* ===== WTC L-WALL (EXAKT) ===== */
+
+export function buildWTCLWalls(w, h) {
+  const r = INCH;
+
+  return [
+    // horisontell del (2 rutor in, 1 ruta ner)
+    [
+      [2 * r, 1 * r],
+      [w - 1 * r, 1 * r]
+    ],
+
+    // vertikal del (1 ruta in, slutar 7/8 in)
+    [
+      [1 * r, 1 * r],
+      [1 * r, h - (1 / 8) * r]
+    ]
+  ];
+}
