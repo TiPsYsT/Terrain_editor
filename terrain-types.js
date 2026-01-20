6daadf73f19c06005325fa21b673c30dfaf6120b
TiPsYsT <alexanderlundgren@hotmail.com>
2026-01-20 15:58:04 +0100

    Update terrain-types.js

--- terrain-types.js
+++ terrain-types.js
@@ -7,56 +7,50 @@ export const INCH = 25.4;
 */
 
 export const TERRAIN_TYPES = {
-  /* ===== RUINS ===== */
-
   two_storey_ruin_12x6: {
     id: "two_storey_ruin_12x6",
-    type: "ruin",
     color: "red",
     w: 12 * INCH,
-    h: 6 * INCH
+    h: 6 * INCH,
+    hasLWalls: true
   },
 
   three_storey_ruin_12x6: {
     id: "three_storey_ruin_12x6",
-    type: "ruin",
     color: "blue",
     w: 12 * INCH,
-    h: 6 * INCH
+    h: 6 * INCH,
+    hasLWalls: true
   },
 
   prototype_ruin_6x5: {
     id: "prototype_ruin_6x5",
-    type: "ruin",
     color: "gray",
     w: 6 * INCH,
-    h: 5 * INCH
+    h: 5 * INCH,
+    hasLWalls: false
   },
-
-  /* ===== CONTAINER ===== */
 
   armoured_container_5x2_5: {
     id: "armoured_container_5x2_5",
-    type: "container",
     color: "gray",
     w: 5 * INCH,
-    h: 2.5 * INCH
+    h: 2.5 * INCH,
+    hasLWalls: false
   }
 };
 
-/* ===== WTC L-WALL (EXAKT) ===== */
-
+/* ===== EXAKT WTC L-VÄGG ===== */
 export function buildWTCLWalls(w, h) {
   const r = INCH;
 
   return [
-    // horisontell del (2 rutor in, 1 ruta ner)
+    // horisontell del
     [
       [2 * r, 1 * r],
       [w - 1 * r, 1 * r]
     ],
-
-    // vertikal del (1 ruta in, slutar 7/8 in)
+    // vertikal del
     [
       [1 * r, 1 * r],
       [1 * r, h - (1 / 8) * r]
