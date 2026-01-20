109db34be26fea0a48a2f62ffb7b679a953170d4
TiPsYsT <alexanderlundgren@hotmail.com>
2026-01-20 15:19:30 +0100

    Update export.js

--- export.js
+++ export.js
@@ -1,1 +1,18 @@
+export function exportJSON(pieces) {
+  const data = {
+    pieces: pieces.map(p => ({
+      type: "ruin",
+      x: Math.round(p.x),
+      y: Math.round(p.y),
+      w: p.w,
+      h: p.h,
+      rotation: p.rotation,
+      walls: p.walls
+    }))
+  };
+
+  const text = JSON.stringify(data, null, 2);
+  navigator.clipboard.writeText(text);
+  alert("JSON copied to clipboard");
+}
 
